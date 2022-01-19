51994a2311ddacca50f4fa125dc5d58137f4ac08
vuqt06 <vuqt@umich.edu>
2022-01-19 15:22:29 -0500

    Initial Commit

---
+++ City Sights Explorer/City_Sights_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_ExplorerApp.swift
+//  City Sights Explorer
+//
+//  Created by Vu Trinh on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ City Sights Explorer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  City Sights Explorer
+//
+//  Created by Vu Trinh on 1/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
